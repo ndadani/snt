3141ef5a4887fc044920cd0ce1c8d1d6f5a10fdf
NDADANI Nada <nada.ndadani@telecomnancy.eu>
2022-06-17 15:15:14 +0200

    .

--- project/oscillator.py
+++ project/oscillator.py
@@ -1,19 +1,25 @@
 import socket
 import numpy
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 6543  # The port used by the server
+def oscillator(omega, k):
+    HOST = "127.0.0.1"  # The server's hostname or IP address
+    PORT = 6543  # The port used by the server
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
 
-s.sendall(bytes(10))
-data = s.recv(1024)
-print(f"Received {data!r}")
-dphi = 10 + 5 * numpy.sin((10 - int.from_bytes(data,"little"))*numpy.pi/180)
-s.sendall(dphi)
+    s.sendall(b"esrgdhtfjygukhi")
+    #s.sendall(bytes(omega))
 
+    data = s.recv(1024)
+    print(f"Received {data!r}")
+    dphi = omega + k * numpy.sin((omega - int.from_bytes(data,"little"))*numpy.pi/180)
+    s.sendall(dphi)
 
+if __name__ == '__main__': 
+    for i in range(10):
+        print(i)
+        oscillator(i,i*10)
 
 
 
