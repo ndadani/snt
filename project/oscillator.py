04d1ac3cf7ad76afe8721bbfcce19d9884e5573e
NDADANI Nada <nada.ndadani@telecomnancy.eu>
2022-06-22 10:35:02 +0200

    multi clients

--- project/oscillator.py
+++ project/oscillator.py
@@ -1,13 +1,17 @@
 import socket
 import numpy
 import struct
+import threading
+import time
 
-def oscillator(omega, k):
-    HOST = "127.0.0.1"  # localhost
-    PORT = 65432  # The port used by the server
 
+SOCKET_AMOUNT = 2
+HOST = "127.0.0.1"  # localhost
+PORT = 9001  # The port used by the server
+
+def oscillator(ip, port, omega, k):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #TCP
-    s.connect((HOST, PORT))
+    s.connect((ip, port))
     send = struct.pack('f',omega)
     s.sendall(send)
     data = struct.unpack('f',s.recv(1024))
@@ -17,13 +21,16 @@ def oscillator(omega, k):
     s.sendall(dphi)
     s.close()
 
-if __name__ == '__main__': 
-    # for i in range(10):
-    #     print(i)
-    #     oscillator(i*10,i)
-    oscillator(100,5)
 
+if __name__ == "__main__":
+    thread_list = []
+    for i in range(SOCKET_AMOUNT):
+        client_thread = threading.Thread(target=oscillator, args=(HOST, PORT, i*10, i))
+        client_thread.setDaemon(True)
+        thread_list.append(client_thread)
+        client_thread.start()
 
-
-
+    waiting = time.time()
+    [x.join() for x in thread_list]
+    
 
--- project/server.py
+++ project/server.py
@@ -1,26 +1,63 @@
+import multiprocessing
 import socket
-import struct
+import time
 
 HOST = "127.0.0.1"
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+PORT = 9001  # Port to listen on (non-privileged ports are > 1023)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen()
-conn, addr = s.accept()
-print(f"Connected by {addr}")
+def handle(connection, address):
+    try:
+        while True:
+            data = connection.recv(1024)
+            connection.sendall(data + ' server time {}'.format(time.time()))
+    except:
+        pass
+    finally:
+        connection.close()
 
-while True:
-    data = conn.recv(1024)
-    if not data:
-        print("break")
-        break
-    print(f"Received {data!r}")
-    conn.sendall(data)
-s.close()
+class Server(object):
+
+    def __init__(self, hostname, port):
+        self.hostname = hostname
+        self.port = port
+
+    def start(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((self.hostname, self.port))
+        self.socket.listen(1)
+
+        while True:
+            conn, address = self.socket.accept()
+            process = multiprocessing.Process(target=handle, args=(conn, address))
+            process.daemon = True
+            process.start()
+
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.bind((HOST, PORT))
+# s.listen()
+# conn, addr = s.accept()
+# print(f"Connected by {addr}")
+
+# while True:
+#     data = conn.recv(1024)
+#     if not data:
+#         print("break")
+#         break
+#     print(f"Received {data!r}")
+#     conn.sendall(data)
+# s.close()
 
 
-
+if __name__ == "__main__":
+    server = Server(HOST, PORT)
+    try:
+        server.start()
+    except:
+        print('Exception')
+    finally:
+        for process in multiprocessing.active_children():
+            process.terminate()
+            process.join()
 
 
 
