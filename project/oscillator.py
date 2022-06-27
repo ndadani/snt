3bc564242fbcd7086c8518bfcb642fffb58b7c5e
NDADANI Nada <nada.ndadani@telecomnancy.eu>
2022-06-27 23:22:12 +0200

    Queuing, separating handlers, threads on the server side

--- project/oscillator.py
+++ project/oscillator.py
@@ -1,59 +1,114 @@
-#TODO Format data size, struct.error: unpack requires a buffer of 4 bytes
-#TODO BrokenPipeError: [Errno 32] Broken pipe
-#TODO ConnectionResetError: [Errno 54] Connection reset by peer
-#TODO wait/get results from other threads (PORT ?)
-#TODO include ids to change data
-
 import socket
 import numpy
-import struct
-import threading
 
 HOST = "127.0.0.1"  # localhost
 PORT = 9001  # The port used by the server
 
-def oscillator(ip, port, omega, k, id):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #TCP
-    try:
-        s.connect((ip, port))
-        omega = float(omega)
-        send = struct.pack('f',omega)
-        #send += b"0" * (4 - len(send))
-        #print(send)
-        s.sendall(send)
-        data = struct.unpack('f',s.recv(1024))
-        print(data)
-        dphi = omega + float(k) * numpy.sin((omega - data[0])*numpy.pi/180)
-        print(dphi)
-        s.sendall(dphi)
-        s.close()
-    except Exception as e:
-        print(e)
+id = input("> id = ")
+omega = input("> omega = ")
+k = input("> k = ")
 
-if __name__ == "__main__":
-    thread_list = dict()
-    SOCKET_AMOUNT = input("> Number of oscillators = ")
-    for i in range(1,int(SOCKET_AMOUNT)+1):
-        id = input("> id = ")
-        omega = input("> omega = ")
-        k = input("> k = ")
-        client_thread = threading.Thread(target=oscillator, args=(HOST, PORT, omega, k, id))
-        client_thread.setDaemon(True)
-        thread_list[id]=client_thread
-    for c in thread_list.values():
-        c.start()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #TCP
 
-    msg = input()
-    if msg=="":
-        [x.join() for x in thread_list.values()]
-    elif msg in thread_list.keys():
-        thread_list[msg].join()
-        omega = input("> omega = ")
-        k = input("> k = ")
-        client_thread = threading.Thread(target=oscillator, args=(HOST, PORT, omega, k, msg))
-        client_thread.setDaemon(True)
-        thread_list[msg]=client_thread
-        client_thread.start()
+try:
+    s.connect((HOST, PORT))
+    s.sendall(str.encode(id+':'+omega+':'+k))
+    osci_list=dict()
+    while True:
+        data = s.recv(2048).decode('utf-8')
+        if data is 'NOW':
+            break
+        data = data.split(':')
+        osci_list[data[0]] = float(data[2]) * numpy.sin((float(omega) - float(data[1]))*numpy.pi/180)
+    for o in osci_list.values():
+        omega = omega + o
+    print(omega)
+    s.sendall(str.encode(id+':'+str(omega)+':'+k))
+    s.close()
+
+except Exception as e:
+    print(e)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #TODO wait/get results from other threads (PORT ?)
+# #TODO Format data size, struct.error: unpack requires a buffer of 4 bytes
+# #TODO BrokenPipeError: [Errno 32] Broken pipe
+# #TODO ConnectionResetError: [Errno 54] Connection reset by peer
+# #TODO include ids to change data
+
+# import socket
+# import numpy
+# import struct
+# import threading
+
+# HOST = "127.0.0.1"  # localhost
+# PORT = 9001  # The port used by the server
+
+# def oscillator(ip, port, omega, k, id):
+#     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #TCP
+#     try:
+#         s.connect((ip, port))
+#         omega = float(omega)
+#         send = struct.pack('f',omega)
+#         #send += b"0" * (4 - len(send))
+#         #print(send)
+#         s.sendall(send)
+#         data = struct.unpack('f',s.recv(1024))
+#         print(data)
+#         dphi = omega + float(k) * numpy.sin((omega - data[0])*numpy.pi/180)
+#         print(dphi)
+#         s.sendall(dphi)
+#         s.close()
+#     except Exception as e:
+#         print(e)
+
+# if __name__ == "__main__":
+#     thread_list = dict()
+#     SOCKET_AMOUNT = input("> Number of oscillators = ")
+#     for i in range(1,int(SOCKET_AMOUNT)+1):
+#         id = input("> id = ")
+#         omega = input("> omega = ")
+#         k = input("> k = ")
+#         client_thread = threading.Thread(target=oscillator, args=(HOST, PORT, omega, k, id))
+#         client_thread.setDaemon(True)
+#         thread_list[id]=client_thread
+#     for c in thread_list.values():
+#         c.start()
+
+#     msg = input()
+#     if msg=="":
+#         [x.join() for x in thread_list.values()]
+
+#     # elif msg in thread_list.keys():
+#     #     [x.join() for x in thread_list.values()]
+#     #     omega = input("> omega = ")
+#     #     k = input("> k = ")
+#     #     client_thread = threading.Thread(target=oscillator, args=(HOST, PORT, omega, k, msg))
+#     #     client_thread.setDaemon(True)
+#     #     thread_list[msg]=client_thread
+#     #     client_thread.start()
+    
+
+
+
+
+
+
+    
+
     
 
 
--- project/server.py
+++ project/server.py
@@ -1,18 +1,31 @@
-import multiprocessing
+import threading
 import socket
+import queue
 
 HOST = "127.0.0.1"
 PORT = 9001  # Port to listen on (non-privileged ports are > 1023)
 
-def handle(connection, address):
-    try:
-        while True:
-            data = connection.recv(1024)
-            connection.sendall(data)
-    except:
-        pass
-    finally:
-        connection.close()
+_sentinel = "STOP" # A sentinel to put on the queue to indicate completion
+
+def handle_recv(connection,q):
+    while True:
+        data = connection.recv(2048)
+        q.put(data)
+        if not data:
+            break
+    q.put(_sentinel)
+    connection.close()
+
+
+def handle_send(connection,q):
+    while True:
+        data = q.get()
+        if data is _sentinel:
+            q.put(_sentinel)
+            break
+        connection.sendall(data)
+    connection.close()
+
 
 class Server(object):
 
@@ -23,27 +36,94 @@ class Server(object):
     def start(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind((self.hostname, self.port))
-        self.socket.listen(1)
+        self.socket.listen()
 
+        q = queue.Queue()
+        thread_list = []
         while True:
             conn, address = self.socket.accept()
-            process = multiprocessing.Process(target=handle, args=(conn, address))
-            process.daemon = True
-            process.start()
+            t1 = threading.Thread(target=handle_recv, args=(conn, q))
+            thread_list.append(t1)
+            t1.daemon = True
+            t2 = threading.Thread(target=handle_send, args=(conn, q))
+            thread_list.append(t2)
+            t2.daemon = True
+            t1.start()
+            t2.start()
+
+            msg=input()
+            if msg=='':
+                break
+            elif msg=='NOW':
+                conn.sendall(str.encode(msg))
+
+        self.socket.close()
+        for t in thread_list:
+            t.join()
 
 if __name__ == "__main__":
     server = Server(HOST, PORT)
     try:
         server.start()
-    except:
-        print('Exception')
-    finally:
-        for process in multiprocessing.active_children():
-            process.terminate()
-            process.join()
+    except Exception as e:
+        print(e)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+# import multiprocessing
+# import socket
+
+# HOST = "127.0.0.1"
+# PORT = 9001  # Port to listen on (non-privileged ports are > 1023)
+
+# def handle(connection, address):
+#     try:
+#         while True:
+#             data = connection.recv(1024)
+#             connection.sendall(data)
+#     except:
+#         pass
+#     finally:
+#         connection.close()
+
+# class Server(object):
+
+#     def __init__(self, hostname, port):
+#         self.hostname = hostname
+#         self.port = port
+
+#     def start(self):
+#         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#         self.socket.bind((self.hostname, self.port))
+#         self.socket.listen()
+
+#         while True:
+#             conn, address = self.socket.accept()
+#             process = multiprocessing.Process(target=handle, args=(conn, address))
+#             process.daemon = True
+#             process.start()
+
+# if __name__ == "__main__":
+#     server = Server(HOST, PORT)
+#     try:
+#         server.start()
+#     except Exception as e:
+#         print(e)
+#     finally:
+#         for process in multiprocessing.active_children():
+#             process.terminate()
+#             process.join()
 
 
 
