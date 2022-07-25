339afea2eefb328be1d14bd52b254a2ea01263d6
NDADANI Nada <nada.ndadani@telecomnancy.eu>
2022-07-25 10:09:07 +0200

    sel

--- project/TCP.py
+++ project/TCP.py
@@ -1,5 +1,7 @@
 import socket
+import selectors
 import time
+import types
 
 class Node(object):
 
@@ -8,24 +10,63 @@ class Node(object):
         self.port = port
 
     def start(self,o,to):
+        sel = selectors.DefaultSelector()
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.socket:
             self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 2048)
             self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             self.socket.bind((self.hostname, self.port))    #if we bind to port 0, the OS will pick an available port
             self.port = self.socket.getsockname()[1]
             self.socket.listen()
-            while True:
-                try :
-                    conn, address = self.socket.accept()
-                    with conn:
-                        data = conn.recv(2048)
-                        # ti = int(round(time.time() * 1000))
-                        # msg = str(o.id)+str(int(o.omega)*(ti-to))
-                        # self.socket.send(str.encode(msg.zfill(17)))
-                        print(data)
-                        conn.send(data)
-                except Exception as e :
-                    conn, address = self.socket.accept()
+            self.socket.setblocking(False)
+            sel.register(self.socket, selectors.EVENT_READ, data=None)
+            try:
+                while True:
+                    events = sel.select(timeout=None)
+                    for key, mask in events:
+                        if key.data is None:
+                            conn, addr = key.fileobj.accept()  # Should be ready to read
+                            print(f"Accepted connection from {addr}")
+                            conn.setblocking(False)
+                            data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+                            events = selectors.EVENT_READ | selectors.EVENT_WRITE
+                            sel.register(conn, events, data=data)
+                        else:
+                            sock = key.fileobj
+                            data = key.data
+                            if mask & selectors.EVENT_READ:
+                                recv_data = sock.recv(1024)  # Should be ready to read
+                                if recv_data:
+                                    data.outb += recv_data
+                                else:
+                                    # print(f"Closing connection to {data.addr}")
+                                    sel.unregister(sock)
+                                    sock.close()
+                            if mask & selectors.EVENT_WRITE:
+                                if data.outb:
+                                    # print(f"Echoing {data.outb!r} to {data.addr}")
+                                    sent = sock.send(data.outb)  # Should be ready to write
+                                    data.outb = data.outb[sent:]
+            except KeyboardInterrupt:
+                print("Caught keyboard interrupt, exiting")
+            finally:
+                sel.close()
+
+
+
+
+
+            # while True:
+            #     try :
+            #         conn, address = self.socket.accept()
+            #         with conn:
+            #             data = conn.recv(2048)
+            #             # ti = int(round(time.time() * 1000))
+            #             # msg = str(o.id)+str(int(o.omega)*(ti-to))
+            #             # self.socket.send(str.encode(msg.zfill(17)))
+            #             print(data)
+            #             conn.send(data)
+            #     except Exception as e :
+            #         conn, address = self.socket.accept()
 
 
 
--- project/main.py
+++ project/main.py
@@ -4,6 +4,9 @@ from queue import Queue
 import socket
 import sys
 
+import selectors
+import types
+
 import numpy
 import TCP
 import threading
@@ -11,7 +14,8 @@ import time
 
 HOST = "127.0.0.1"  #local host
 PORT = 0
-loop = 3
+LOOP = 2
+
 
 class Oscillator(object):
 
@@ -32,32 +36,84 @@ class Oscillator(object):
                 time.sleep(0.001)
             self.ports.append(server.port)
         self.ports = self.ports + [0]*(nodes-1-sock_count)
-        print(self.ports)
+        # print(self.ports)
 
-    def threaded_connection(self,signal,port,to):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:   #TCP
-            s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 2048)
-            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            try:
-                s.connect((HOST, port))
-            except Exception as e:
-                print(e)
 
-            while signal:
+    def threaded_connection(self,port,to):
+        ti = int(round(time.time() * 1000))
+        # msg = str(int(self.omega)*(ti-to))
+        messages = [str.encode(str(i)) for i in range(0,LOOP)]
+        sel = selectors.DefaultSelector()
+        conn_count=1
+        for i in range(0, conn_count):
+            connid = i+1
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:   #TCP
+                s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 2048)
+                s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                s.setblocking(False)
+                s.connect_ex((HOST, port))
+                events = selectors.EVENT_READ | selectors.EVENT_WRITE
+                data = types.SimpleNamespace(
+                    connid=connid,
+                    msg_total=sum(len(m) for m in messages),
+                    recv_total=0,
+                    messages=messages.copy(),
+                    outb=b"",
+                )
+                sel.register(s, events, data=data)
                 try:
-                    ti = int(round(time.time() * 1000))
-                    msg = "S"+str(self.id)+":"+str(int(self.omega)*(ti-to))+"E"
-                    # while sys.getsizeof(msg)!= 2048:
-                    #     msg = msg.zfill(1)
-                    print(sys.getsizeof(msg))
-                    s.sendall(str.encode(msg))
-                    data = s.recv(2048)
-                    print(data.decode()+str(threading.get_ident()))
-                except Exception as e:
-                    print("You have been disconnected from the server")
-                    print(e)
-                    signal = False
-                    break
+                    while True:
+                        events = sel.select(timeout=None)
+                        for key, mask in events:
+                            if key.data is None:
+                                conn, addr = key.fileobj.accept()  # Should be ready to read
+                                print(f"Accepted connection from {addr}")
+                                conn.setblocking(False)
+                                data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+                                events = selectors.EVENT_READ | selectors.EVENT_WRITE
+                                sel.register(conn, events, data=data)
+                            else:
+                                sock = key.fileobj
+                                data = key.data
+                                if mask & selectors.EVENT_READ:
+                                    recv_data = sock.recv(1024)  # Should be ready to read
+                                    if recv_data:
+                                        print(f"Received {recv_data!r} from connection {data.connid}")
+                                        data.recv_total += len(recv_data)
+                                    if not recv_data or data.recv_total == data.msg_total:
+                                        print(f"Closing connection {data.connid}")
+                                        sel.unregister(sock)
+                                        sock.close()
+                                if mask & selectors.EVENT_WRITE:
+                                    if not data.outb and data.messages:
+                                        data.outb = data.messages.pop(0)
+                                    if data.outb:
+                                        print(f"{key.fd} Sending {data.outb!r} to connection {data.connid}")
+                                        sent = sock.send(data.outb)  # Should be ready to write
+                                        data.outb = data.outb[sent:]
+                except KeyboardInterrupt:
+                    print("Caught keyboard interrupt, exiting")
+                finally:
+                    sel.close()
+
+                # while signal:
+                #     try:
+                #         ti = int(round(time.time() * 1000))
+                #         msg = "S"+str(self.id)+":"+str(int(self.omega)*(ti-to))+"E"
+                #         count = 1
+                #         # while sys.getsizeof(msg)!= 64:
+                #         #     msg = msg.zfill(len(msg)+count)
+                #         #     count+=1
+                        
+                #         # print(sys.getsizeof(msg))
+                #         s.sendall(str.encode(msg))
+                #         data = s.recv(2048)
+                #         print(data.decode()+str(threading.get_ident()))
+                #     except Exception as e:
+                #         print("You have been disconnected from the server")
+                #         print(e)
+                #         signal = False
+                #         break
         
 def distribute(l:list , N:int):
     k=0
@@ -84,13 +140,12 @@ def replica(ports_list:list, id:int , omega:int , k:int , q:Queue , o_list:list 
     #     wait
 
     for port in ports_list[o.id:o.id+nodes-1]:
-            t = threading.Thread(target=o.threaded_connection, args=(True,port,to))
+            t = threading.Thread(target=o.threaded_connection, args=(port,to))
             t.daemon = True
             t.start()
     
 if __name__ == "__main__":
     to = int(round(time.time() * 1000))
-    print(to)
     # IPC
     q = multiprocessing.Queue()
     o_list = []
@@ -103,11 +158,11 @@ if __name__ == "__main__":
             try :
                 data = f.readline().split(',')
                 # process = multiprocessing.Process(target=replica, args=(shared_list,n,data[0],data[1],q,o_list,nodes))
-                process = threading.Thread(target=replica, args=(shared_list,n,data[0],data[1],q,o_list,nodes,to))     # pour l'instant, après on tente un script processus/shell
-                process.daemon = True
-                process.start()
+                t = threading.Thread(target=replica, args=(shared_list,n,data[0],data[1],q,o_list,nodes,to))     # pour l'instant
+                t.daemon = True
+                t.start()
                 o_list.append(q.get())
-                process.join()
+                t.join()
             except Exception as e:
                 print(e)
 
